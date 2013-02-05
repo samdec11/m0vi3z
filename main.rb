077a5e04ec350d4050005c0cecbb96a219aa4495
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-02-05 09:35:56 -0500

    fixed bugs around apostrophes

--- main.rb
+++ main.rb
@@ -11,8 +11,8 @@ end
 
 print "Enter a movie: "
 title = gets.chomp.downcase.split
-movie = HTTParty.get("http://www.omdbapi.com/?t=#{title.join('+')}")
-movie_hash = JSON(movie.body)
+movie = HTTParty.get("http://www.omdbapi.com/?t=#{title.join('+')}").gsub("'", "")
+movie_hash = JSON(movie)
 
 begin
   sql = "insert into movies (#{movie_hash.keys.join(', ')}) values ('#{movie_hash.values.join('\', \'')}')"
